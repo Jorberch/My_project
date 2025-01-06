323b92fd83419348044d05c4bed8610618b86444
Jorberch <jorberch@hotmail.com>
2025-01-06 02:38:49 -0500

    change name fig

--- app.py
+++ app.py
@@ -16,38 +16,37 @@ if st.button('Creación de un histograma para el conjunto de datos de anuncios d
 # Crear un botón para generar un gráfico de dispersión
 if st.button("Mostrar Diagrama de Dispersión (Millaje vs Precio)"):
     # Construir el gráfico de dispersión
-    fig = px.scatter(car_data, x='odometer', y='price', 
+    fig_1 = px.scatter(car_data, x='odometer', y='price', 
                      title='Relación entre Millaje y Precio',
                      labels={'odometer': 'Millaje', 'price': 'Precio'},
                      color='condition')
     # Mostrar el gráfico en la aplicación
-    st.plotly_chart(fig)
+    st.plotly_chart(fig_1)
 
 # Casilla de verificación para el histograma
 if st.checkbox("Mostrar Histograma de Precios"):
     # Construir el histograma
-    fig = px.histogram(car_data, x='price', nbins=50, title='Distribución de Precios de los Vehículos')
+    fig_2= px.histogram(car_data, x='price', nbins=50, title='Distribución de Precios de los Vehículos')
     # Mostrar el gráfico en la aplicación
-    st.plotly_chart(fig)
+    st.plotly_chart(fig_2)
 
 # Casilla de verificación para el gráfico de dispersión
 if st.checkbox("Mostrar Diagrama de Dispersión (Millaje vs Precio)"):
     # Construir el gráfico de dispersión
-    fig = px.scatter(car_data, x='odometer', y='price', 
+    fig_3 = px.scatter(car_data, x='odometer', y='price', 
                      title='Relación entre Millaje y Precio',
                      labels={'odometer': 'Millaje', 'price': 'Precio'},
                      color='condition')
     # Mostrar el gráfico en la aplicación
-    st.plotly_chart(fig)
+    st.plotly_chart(fig_3)
 
 # Construir el gráfico Distribución de precios por condición, tipo y modelo
-    fig = px.sunburst(
-    car_data,
-    path=["condition", "type", "model"],
-    values="price",
-    color="condition",
-    title="Distribución de precios por condición, tipo y modelo",
+    fig_dist = px.sunburst(car_data,
+                           path=["condition", "type", "model"],
+                           values="price",
+                           color="condition",
+                           title="Distribución de precios por condición, tipo y modelo",
 )
 
 # Mostrar el gráfico en Streamlit
-st.plotly_chart(fig)
+st.plotly_chart(fig_dist)
