de44c4efc2a8986bf3b66149292137f3ccc48402
Jorberch <jorberch@hotmail.com>
2025-01-06 01:48:13 -0500

    update app

---
+++ app.py
@@ -0,0 +1,42 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+
+car_data = pd.read_csv('vehicles_us.csv') #leer el dataframe
+st.header("Análisis Exploratorio de Vehículos") #Crear encabezado
+st.write("Vista previa del conjunto de datos:") #vista previa de datos
+st.write(car_data.head())
+
+# Crear un botón para generar un histograma
+if hist_button:
+    st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
+    fig = px.histogram(car_data, x='odometer', nbins=50, title='Distribución de Precios de los Vehículos')
+    # Mostrar el gráfico en la aplicación
+    st.plotly_chart(fig, use_container_width=True)
+
+# Crear un botón para generar un gráfico de dispersión
+if st.button("Mostrar Diagrama de Dispersión (Millaje vs Precio)"):
+    # Construir el gráfico de dispersión
+    fig = px.scatter(car_data, x='odometer', y='price', 
+                     title='Relación entre Millaje y Precio',
+                     labels={'odometer': 'Millaje', 'price': 'Precio'},
+                     color='condition')
+    # Mostrar el gráfico en la aplicación
+    st.plotly_chart(fig)
+
+# Casilla de verificación para el histograma
+if st.checkbox("Mostrar Histograma de Precios"):
+    # Construir el histograma
+    fig = px.histogram(car_data, x='price', nbins=50, title='Distribución de Precios de los Vehículos')
+    # Mostrar el gráfico en la aplicación
+    st.plotly_chart(fig)
+
+# Casilla de verificación para el gráfico de dispersión
+if st.checkbox("Mostrar Diagrama de Dispersión (Millaje vs Precio)"):
+    # Construir el gráfico de dispersión
+    fig = px.scatter(car_data, x='odometer', y='price', 
+                     title='Relación entre Millaje y Precio',
+                     labels={'odometer': 'Millaje', 'price': 'Precio'},
+                     color='condition')
+    # Mostrar el gráfico en la aplicación
+    st.plotly_chart(fig)
